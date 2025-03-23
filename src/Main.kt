fa41be6b0ef86d1497df0a9e56f8933c362ecea2
waimea-dmneth <dmneth@waimea.school.nz>
2025-03-24 11:02:11 +1300

    dsfsd

--- src/Main.kt
+++ src/Main.kt
@@ -18,61 +18,116 @@ const val EMPTY = 0
 const val SILVER = 1
 const val GOLD = 2
 // --               ^
+// display keys           v
+const val SHOWCOINS = 1
+// --                     ^
 var coinStash: Int = 40
 var coinPot: Int = 5
-val Game = mutableListOf<Any>()
+val game = mutableListOf<Any>()
 //initiate vars and vals -- plan
 fun main() {
-    SetUp()
-    DisplayGame()
+    setUp()
+    displayGame(0)
     while (coinStash > 0) { // loop through turns till gold coin is removed -- plan
         while (true) {
-            SetUpGame() // parameter, change to a number decided by player later ------ !!!
-            DisplayGame()
+            setUpGame()
+            displayGame(0)
+            doTurn(1) // change player param eventually
             break // dont keep
+            //coinPot = 5
+
         }
         break // dont keep
     }
 
 
     // do something for winner -- plan
-
 }
 
-fun SetUp() {
-    for (i in 0..BOARD_LENGTH-1) {
-        Game.add(EMPTY)
+fun setUp() {
+    for (i in 0..<BOARD_LENGTH) {
+        game.add(EMPTY)
     }
 }
 
-fun SetSlot(index: Int, setTo: Any): Boolean {
-    if (index < 0 || index >= Game.size) return false
-    if (Game[index] != EMPTY) return false
-    Game[index] = setTo
+fun setSlot(index: Int, setTo: Any): Boolean {
+    if (index < 0 || index >= game.size) return false
+    if (game[index] != EMPTY) return false
+    game[index] = setTo
     return true
 }
 
-fun SetUpGame(): Boolean {
-    if (coinPot > BOARD_LENGTH) return false
+fun getAction(player: Int) {
+    println()
+    println("What Coin Do You Wish To Move? (1-6 in the [])")
+    displayGame(SHOWCOINS)
+    print("Coin?: ")
+    var coinToMove = readln().toInt()
+    for (i in 0..BOARD_LENGTH-1) {
+        if (i != EMPTY) coinToMove--
+        if (coinToMove == 0) continue
+        coinToMove = i
+        break
+    }
+    println("Pick a Spot to move to")
+    for (i in 0..coinToMove) {}
+        print()
+    // get the action and show possible moves with chosen piece
+    // return action with player num
+}
+
+fun doTurn(player: Int) {
+    getAction(player)
+    // get action
+    // play action
+}
+
+fun setUpGame(): Boolean {
+    var lowestSlot = 15
+    if (coinPot > BOARD_LENGTH || coinPot == 0) return false
     while (coinPot > 0) {
-        for (i in 1..BOARD_LENGTH) {
-            if (coinPot <= 0) break
-            if (Random.nextInt(1,10) < 5) continue
-            val result = SetSlot(i, SILVER)
-            if (!result) continue
+        for (i in BOARD_LENGTH downTo 1) {               // checks for
+            if (coinPot <= 0) break                           // if coinpot is empty
+            if (Random.nextInt(1,10) < 9) continue // randomly set
+            val result = setSlot(i, SILVER)                  // checks slot is empty V
+            if (!result) continue                           //                       ^
             coinPot--
+            if (i < lowestSlot) lowestSlot = i
+            //println(lowestSlot)
         }
     }
-    var goldSlot = Random.nextInt(1,BOARD_LENGTH-1)
-    while (Game[goldSlot] != EMPTY) goldSlot = Random.nextInt(1,BOARD_LENGTH-1)
-    SetSlot(goldSlot, GOLD)
-
+    var goldSlot = Random.nextInt(lowestSlot+1,BOARD_LENGTH-1)
+    while (game[goldSlot] != EMPTY) goldSlot = Random.nextInt(lowestSlot+1,BOARD_LENGTH-1)
+    setSlot(goldSlot, GOLD)
 
     return true
 }
 
-fun DisplayGame() {
-    println(Game)
+fun displayGame(type: Int) {
+    when (type) {
+        SHOWCOINS -> {
+            var count:Int = 1
+            for (i in 0..<BOARD_LENGTH) {
+                when (game[i]) {
+                    EMPTY -> print("|"+" $EMPTY ".grey())
+                    SILVER -> {
+                        print("| $SILVER [$count] ")
+                        count++
+                    }
+                    GOLD -> {
+                        print("|"+" $GOLD [$count] ".yellow())
+                        count++
+                    }
+                }
+
+            }
+            println("|")
+        }
+        else -> {
+            println(game)
+        }
+    }
+
 }
 /**
  * functions plans
